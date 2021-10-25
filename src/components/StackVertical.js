bdf678386ff0fb28051b39263b863daa88a20ade
spitchell <mr.spitchell@gmail.com>
2021-10-25 20:30:39 +0100

    implement better filtering system

--- src/components/StackVertical.js
+++ src/components/StackVertical.js
@@ -3,14 +3,22 @@ import { LitElement, html, css } from 'lit';
 import resetCSS from '../js/reset-css.js';
 import data from '../data.json';
 
+data.forEach(job =>
+{
+  job.tags = [];
+  job.tags.push(job.role, job.level);
+  job.languages.forEach(language => job.tags.push(language));
+  job.tools.forEach(tool => job.tags.push(tool));
+});
 
 import './JobListing.js';
 
 export class StackVertical extends LitElement
 {
-  constructor() {
+  constructor()
+  {
     super();
-    this.filterTerms = ["Frontend"];
+    this.filterTags = [];
   }
 
   static get styles() /* what does static do for me here? required? */
@@ -33,43 +41,46 @@ export class StackVertical extends LitElement
 
 
   // The filtering should be done in a controller or something - the StackVertical component should be purely presentational.
-  filtered(data){
-    const jobProperties = [];
-    jobProperties.push(job.role,job.level);
-    job.languages.forEach(language => jobProperties.push(language));
-    job.tools.forEach(tool => jobProperties.push(tool));
-    console.log(jobProperties);
-    
+  filtered(data)
+  {
+    if (this.filterTags.length > 0) {
+      return data.filter(job => this.filterTags.every(tag => job.tags.includes(tag)));
+    }
+    return data;
   }
 
-  updateFilters(e) {
+  updateFilters(e)
+  {
     const filterNameEvent = e.detail.text;
-    !this.filterTerms.includes(filterNameEvent) ? this.filterTerms.push(filterNameEvent) : this.filterTerms = this.filterTerms.filter(term => term !== filterNameEvent);
+    !this.filterTags.includes(filterNameEvent) ? this.filterTags.push(filterNameEvent) : this.filterTags = this.filterTags.filter(term => term !== filterNameEvent);
+    this.requestUpdate();
   }
 
   render()
   {
-    return html`  
+    return html`
     <!-- should be li in a ul, probably -->
-      <div class="stack" @emit-filter=${(e)=>this.updateFilters(e)}>
-        ${
-          data.map((job) => html`
+      <ul>
+        ${ this.filterTags.map(tag => html`<filter-button text=${ tag }>`) }
+      </ul>
+      <div class="stack" @emit-filter=${ (e) => this.updateFilters(e) }>
+        ${ this.filtered(data).map(job => html`
           <job-listing
-            company=${job.company}
-            logo=${job.logo}
-            ?new=${job.new}
-            ?featured=${job.featured}
-            position=${job.position}
-            role=${job.role}
-            level=${job.level}
-            postedAt=${job.postedAt}
-            contract=${job.contract}
-            location=${job.location}
-            .languages=${job.languages}
-            .tools=${job.tools}>
+            company=${ job.company }
+            logo=${ job.logo }
+            ?new=${ job.new }
+            ?featured=${ job.featured }
+            position=${ job.position }
+            role=${ job.role }
+            level=${ job.level }
+            postedAt=${ job.postedAt }
+            contract=${ job.contract }
+            location=${ job.location }
+            .languages=${ job.languages }
+            .tools=${ job.tools }>
           </job-listing>
           `)
-        }
+      }
       </div>
     `;
   }
---
+++ src/components/TagLabel.js
@@ -0,0 +1,65 @@
+import { LitElement, html, css } from 'lit';
+import resetCSS from '../js/reset-css.js'; /* needed? */
+
+export class TagLabel extends LitElement
+{
+  static get properties() {
+    return {
+      text: {type: String},
+      filterType: {type: String}
+    }
+  }
+  static get styles()
+  {
+    return [
+      resetCSS, /* button only??? */
+      css`
+        button {
+          border-radius: 1.5rem;
+          padding: 0.9rem 1rem 0.75rem;
+          border-radius: 0.4rem;
+          color: var(--color__teal);
+          background-color: var(--color__teal--light);
+          transition: var(--transition__out);
+          border: none;
+          font-size: 1.1rem;
+          font-weight: 700;
+        }
+        button:hover,
+        button:focus-visible {
+          background-color: var(--color__teal);
+          color: var(--color__white);
+          transition: var(--transition__in);
+          outline-color: var(--color__teal);
+          outline-offset: 4px;
+        }
+        @media (min-width: 1024px) {
+          button {
+            font-size: 1.3rem;
+          }
+        }
+      `
+    ];
+  }
+  announceFilter(filterType,text) {
+    this.dispatchEvent(new CustomEvent('announce-filter', {
+    composed: true,
+    bubbles: true,
+    detail: {
+      filterType: filterType,
+      text: text
+    } }));
+  }
+
+  render()
+  {
+    return html`
+      <button @click=${()=>this.announceFilter(this.filterType,this.text)}>
+          ${this.text}
+      </button>
+    `;
+  }
+}
+
+
+customElements.define('tag-label', TagLabel);
