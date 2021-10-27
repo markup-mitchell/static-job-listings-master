9137abbd456d3328727548d2fd5089d8a99616ba
Mark Mitchell <mark.mitchell@visitscotland.com>
2021-10-27 15:45:51 +0100

    extracted filter list into component

---
+++ src/components/FilterTags.js
@@ -0,0 +1,87 @@
+import { LitElement, html, css } from 'lit';
+
+import "./TagDismiss.js";
+import "./WrapperCard";
+
+export class FilterTags extends LitElement {
+  static get properties() {
+    return {
+      filterTags: { type: Array }
+    };
+  }
+  static get styles() {
+    return css`
+      .tag-wrapper {
+        display: flex;
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        background-color: var(--color__white);
+        gap: 1.5rem;
+        grid-column: 2;
+        grid-row: 2;
+      }
+      li {
+        display: inline-block;
+      }
+      h1 {
+        /* sr-only */
+        position: absolute;
+        width: 1px;
+        height: 1px;
+        padding: 0;
+        margin: -1px;
+        overflow: hidden;
+        clip: rect(0, 0, 0, 0);
+        white-space: nowrap;
+        border-width: 0;
+      }
+      article {
+        width: 100%;
+        display: grid;
+        grid-template-columns: 2.4rem 1fr min-content 2.4rem;
+        grid-template-rows: 2.4rem 1fr 2.4rem;
+      }
+      wrapper-card {
+        grid-column: 1 / -1;
+        grid-row: 1 / span 3;
+      }
+
+      button{
+        grid-row: 2;
+        grid-column: 3;
+        white-space: nowrap;
+      }
+
+      @media (min-width: 600px){
+        article {
+        grid-template-columns: 4rem 1fr min-content 4rem;
+      }
+      }
+
+    `;
+  }
+
+  attributeChangedCallback() {
+    this.requestUpdate();
+  }
+  
+  render() {
+    return html`
+    <article>
+      <wrapper-card></wrapper-card>
+      <ul class="tag-wrapper">
+        ${
+          this.filterTags.map( tag => html`
+          <li>
+            <tag-dismiss text=${tag}></tag-dismiss>
+          </li>
+          `)
+        }
+      </ul>
+  </article>
+  `;
+  }
+}
+
+customElements.define( 'filter-tags', FilterTags );
--- src/components/StackVertical.js
+++ src/components/StackVertical.js
@@ -3,43 +3,28 @@ import { LitElement, html, css } from 'lit';
 import './JobListing.js';
 import data from '../data.json';
 import './TagDismiss.js';
+import './FilterTags';
 
-data.forEach(job =>
-{
+data.forEach( job => {
   job.tags = [];
-  job.tags.push(job.role, job.level);
-  job.languages.forEach(language => job.tags.push(language));
-  job.tools.forEach(tool => job.tags.push(tool));
-});
+  job.tags.push( job.role, job.level );
+  job.languages.forEach( language => job.tags.push( language ) );
+  job.tools.forEach( tool => job.tags.push( tool ) );
+} );
 
 
-export class StackVertical extends LitElement
-{
-  constructor()
-  {
+export class StackVertical extends LitElement {
+  constructor() {
     super();
     this.filterTags = [];
   }
 
-  static get styles() /* what does static do for me here? required? */
-  {
+  static get styles() /* what does static do for me here? required? */ {
     return css`
-      ul {
-        list-style: none;
-        display: flex;
-        background-color: var(--color__white);
-        padding: 2rem;
-        border-radius: 0.5rem;
-        box-shadow: 0px 15px 20px -5px rgba(13, 113, 130, 0.15);
-        gap: 1.5rem;
-        flex-wrap: wrap;
-      }
       .stack {
         display: flex;
         flex-direction: column;
         gap: 1.6rem;
-        /* opacity: 0; */
-        /* animation: fadeIn 1s forwards; */
       }
 
       @keyframes fadeIn {
@@ -57,41 +42,32 @@ export class StackVertical extends LitElement
       }`;
   }
 
-
   // The filtering should be done in a controller or something - the StackVertical component should be purely presentational.
-  filtered(data)
-  {
-    if (this.filterTags.length > 0) {
-      return data.filter(job => this.filterTags.every(tag => job.tags.includes(tag)));
+  filtered( data ) {
+    if ( this.filterTags.length > 0 ) {
+      return data.filter( job => this.filterTags.every( tag => job.tags.includes( tag ) ) );
     }
     return data;
   }
 
-  updateFilters(e)
-  {
+  updateFilters( e ) {
     const filterNameEvent = e.detail.text;
-    !this.filterTags.includes(filterNameEvent) ? this.filterTags.push(filterNameEvent) : this.filterTags = this.filterTags.filter(term => term !== filterNameEvent);
+    !this.filterTags.includes( filterNameEvent ) ? this.filterTags = [...this.filterTags, filterNameEvent] : this.filterTags = this.filterTags.filter( term => term !== filterNameEvent );
     this.requestUpdate();
   }
 
-  render()
-  {
+  render() {
     return html`
-    <!-- should be li in a ul, probably -->
-    ${ this.filterTags.length > 0 ?
-        html`<ul @emit-filter=${ (e) => this.updateFilters(e) }>
-      ${ this.filterTags.map(tag => html`
-      <li>
-        <tag-dismiss text=${ tag }></tag-dismiss>
-      </li>
-      ` ) }
-    </ul>`
-        : null
-      }
-    <div class="stack" @emit-filter=${ (e) => this.updateFilters(e) }>
-      ${ this.filtered(data).map(job => html`
-      <job-listing company=${ job.company } logo=${ job.logo } ?new=${ job.new } ?featured=${ job.featured } position=${ job.position } role=${ job.role } level=${ job.level } postedAt=${ job.postedAt } contract=${ job.contract }
-        location=${ job.location } .languages=${ job.languages } .tools=${ job.tools }>
+    <filter-tags
+      @emit-filter=${( e ) => this.updateFilters( e )}
+      .filterTags=${this.filterTags}>
+    </filter-tags>
+
+    <div class="stack" @emit-filter=${( e )=> this.updateFilters( e )}>
+      ${this.filtered( data ).map( job => html`
+      <job-listing company=${job.company} logo=${job.logo} ?new=${job.new} ?featured=${job.featured} position=${
+       job.position} role=${job.role} level=${job.level} postedAt=${job.postedAt} contract=${job.contract}
+        location=${job.location} .languages=${job.languages} .tools=${job.tools}>
       </job-listing>
       `)
       }
@@ -100,5 +76,5 @@ export class StackVertical extends LitElement
   }
 }
 
-customElements.define('stack-vertical', StackVertical);
+customElements.define( 'stack-vertical', StackVertical );
 
