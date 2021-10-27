20525ed8720b7ffa7bd5f53deac6cb9bac405847
spitchell <mr.spitchell@gmail.com>
2021-10-27 09:48:22 +0100

    more transition attempts

--- src/components/StackVertical.js
+++ src/components/StackVertical.js
@@ -4,23 +4,27 @@ import './JobListing.js';
 import data from '../data.json';
 import './TagDismiss.js';
 
-data.forEach( job => {
+data.forEach(job =>
+{
   job.tags = [];
-  job.tags.push( job.role, job.level );
-  job.languages.forEach( language => job.tags.push( language ) );
-  job.tools.forEach( tool => job.tags.push( tool ) );
-} );
+  job.tags.push(job.role, job.level);
+  job.languages.forEach(language => job.tags.push(language));
+  job.tools.forEach(tool => job.tags.push(tool));
+});
 
 
-export class StackVertical extends LitElement {
-  constructor() {
+export class StackVertical extends LitElement
+{
+  constructor()
+  {
     super();
     this.filterTags = [];
   }
 
-  static get styles() /* what does static do for me here? required? */ {
+  static get styles() /* what does static do for me here? required? */
+  {
     return css`
-      ul { 
+      ul {
         list-style: none;
         display: flex;
         background-color: var(--color__white);
@@ -34,6 +38,14 @@ export class StackVertical extends LitElement {
         display: flex;
         flex-direction: column;
         gap: 1.6rem;
+        /* opacity: 0; */
+        /* animation: fadeIn 1s forwards; */
+      }
+
+      @keyframes fadeIn {
+        to {
+          opacity: 1;
+        }
       }
       @media (min-width: 800px) {
         ul {
@@ -47,37 +59,39 @@ export class StackVertical extends LitElement {
 
 
   // The filtering should be done in a controller or something - the StackVertical component should be purely presentational.
-  filtered( data ) {
-    if ( this.filterTags.length > 0 ) {
-      return data.filter( job => this.filterTags.every( tag => job.tags.includes( tag ) ) );
+  filtered(data)
+  {
+    if (this.filterTags.length > 0) {
+      return data.filter(job => this.filterTags.every(tag => job.tags.includes(tag)));
     }
     return data;
   }
 
-  updateFilters( e ) {
+  updateFilters(e)
+  {
     const filterNameEvent = e.detail.text;
-    !this.filterTags.includes( filterNameEvent ) ? this.filterTags.push( filterNameEvent ) : this.filterTags = this.filterTags.filter( term => term !== filterNameEvent );
+    !this.filterTags.includes(filterNameEvent) ? this.filterTags.push(filterNameEvent) : this.filterTags = this.filterTags.filter(term => term !== filterNameEvent);
     this.requestUpdate();
   }
 
-  render() {
+  render()
+  {
     return html`
     <!-- should be li in a ul, probably -->
-    ${this.filterTags.length > 0 ?
-      html`<ul @emit-filter=${( e )=> this.updateFilters( e )}>
-      ${this.filterTags.map( tag => html`
+    ${ this.filterTags.length > 0 ?
+        html`<ul @emit-filter=${ (e) => this.updateFilters(e) }>
+      ${ this.filterTags.map(tag => html`
       <li>
-        <tag-dismiss text=${tag}></tag-dismiss>
+        <tag-dismiss text=${ tag }></tag-dismiss>
       </li>
-      ` )}
+      ` ) }
     </ul>`
         : null
       }
-    <div class="stack" @emit-filter=${( e )=> this.updateFilters( e )}>
-      ${this.filtered( data ).map( job => html`
-      <job-listing company=${job.company} logo=${job.logo} ?new=${job.new} ?featured=${job.featured} position=${
-       job.position} role=${job.role} level=${job.level} postedAt=${job.postedAt} contract=${job.contract}
-        location=${job.location} .languages=${job.languages} .tools=${job.tools}>
+    <div class="stack" @emit-filter=${ (e) => this.updateFilters(e) }>
+      ${ this.filtered(data).map(job => html`
+      <job-listing company=${ job.company } logo=${ job.logo } ?new=${ job.new } ?featured=${ job.featured } position=${ job.position } role=${ job.role } level=${ job.level } postedAt=${ job.postedAt } contract=${ job.contract }
+        location=${ job.location } .languages=${ job.languages } .tools=${ job.tools }>
       </job-listing>
       `)
       }
@@ -86,5 +100,5 @@ export class StackVertical extends LitElement {
   }
 }
 
-customElements.define( 'stack-vertical', StackVertical );
+customElements.define('stack-vertical', StackVertical);
 
